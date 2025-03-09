85341163475d6f37bf8a8f743c6bfc361ef4d5c3
paulo-feitosaa <p.rafael1996@gmail.com>
2025-03-09 14:19:19 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import math
+
+st.set_page_config(
+    layout="centered",
+    page_title='Volume do Silo',
+)
+
+# Título do aplicativo
+st.title("Volume do Silo")
+
+altura = st.number_input("", min_value=0.0, step=0.1, max_value=3.247, value=None, placeholder='Digite o valor de H')  # Campo de entrada do raio
+
+st.image('triangulo45.jpg')
+
+# Função para calcular o volume da esfera
+def calcular_volume(altura):
+    return (math.pi / 72) * (math.sqrt(3) * 3.75 - 2 * altura)**3 
+
+# Botão para cálculo e exibição do resultado
+if st.button("Calcular Volume"):
+    if altura != None:
+        if altura >= 0 and altura < 3.247:
+            volume = calcular_volume(altura)
+            st.success(f"O volume do silo é: {volume:.2f} m³")
+    else:
+        st.warning("Por favor, insira um valor válido para a altura.")
+
