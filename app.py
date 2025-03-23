443bc83d7c791949c56b77332a8ae1568212ea2e
paulo-feitosaa <p.rafael1996@gmail.com>
2025-03-23 19:59:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,26 @@ st.set_page_config(
 
 # Título do aplicativo
 st.title("Volume do Silo")
+st.image('cone45.jpg')
+h = st.number_input("Digite a leitura do sensor em mm", min_value=0.0, step=1.0,  value=None, placeholder='Digite o valor de h em mm') 
 
-altura = st.number_input("", min_value=0.0, step=0.1, max_value=3.247, value=None, placeholder='Digite o valor de H em m')  # Campo de entrada do raio
+D = st.number_input("Digite o valor de D em mm", min_value=0.0, step=1.0,  value=3750.0)  # Campo de entrada do raio
 
-st.image('triangulo45.jpg')
+H = st.number_input("Digite o valor de H em mm", min_value=0.0, step=1.0,  value=3000.0) 
+
+d = st.number_input("Digite o valor de d em mm", min_value=0.0, step=1.0,  value=200.0) 
+
 
 # Função para calcular o volume da esfera
-def calcular_volume(altura):
-    return (math.pi / 72) * (math.sqrt(3) * 3.75 - 2 * altura)**3 
+def calcular_volume(h):
+    c = d + (H - h) * (D - d) / H
+    return (math.pi / 12) * (H - h) * (c*c + d*d + c*d)
 
 # Botão para cálculo e exibição do resultado
 if st.button("Calcular Volume"):
-    if altura != None:
-        if altura >= 0 and altura < 3.247:
-            volume = calcular_volume(altura)
+    if h != None:
+        if h >= 0:
+            volume = calcular_volume(h) * 1e-9
             st.success(f"O volume do silo é: {volume:.2f} m³")
     else:
         st.warning("Por favor, insira um valor válido para a altura.")
